f1fa026c6c9c55b813bb395eeb3ba68d72aea19b
Daniel Collione <danielcollione@MacBook-Air-de-suporte.local>
2022-01-06 11:40:51 -0300

    tarefa dia 6/1/22

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("<html><body>Portal de Noticias</body></html>")
-})
+    res.render('home/index');
+});
+
+app.get('/formulario_inclusao_noticia', (req, res) => {
+    res.render('admin/form_add_noticia');
+});
+
+app.get('/noticias', (req, res) => {
+    res.render('noticias/noticias');
+});
 
 app.listen(3000, () => {
     console.log('the server is online on port 3000!');
