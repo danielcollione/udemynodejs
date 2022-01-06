a9a81066ee0209d4add12d7d10b39eab97689629
Daniel Collione <danielcollione@MacBook-Air-de-suporte.local>
2022-01-06 13:49:11 -0300

    continuando

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('home/index');
-});
-
-app.get('/formulario_inclusao_noticia', (req, res) => {
-    res.render('admin/form_add_noticia');
-});
-
-app.get('/noticias', (req, res) => {
-    res.render('noticias/noticias');
-});
+require('./app/routes/noticias')(app);
+require('./app/routes/home')(app);
+require('./app/routes/formulario-inclusao-noticia')(app);
 
 app.listen(3000, () => {
     console.log('the server is online on port 3000!');
